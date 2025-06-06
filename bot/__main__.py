7414806b04bfafb3fe20d9baa263e882175841cd
Dinara Urazova <dinara.urazova88@gmail.com>
2025-06-06 19:19:59 +0500

    add keyboard (same commit)

---
+++ bot/__main__.py
@@ -0,0 +1,60 @@
+import json
+from bot.api_client import send_message, delete_message, get_updates
+import time
+
+next_update_id = 0
+
+while True:
+    try:  # ask Telegram for new updates
+        response = get_updates(next_update_id)
+        data = response.json()
+        print(data)
+        updates = data["result"]
+
+        for update in updates:
+            next_update_id = update["update_id"] + 1
+            message = update.get("message")
+            if not message:
+                continue
+
+            message_text = message.get("text")
+            chat_id = message["chat"]["id"]
+            message_id = message.get("message_id")
+            if not chat_id or not message_text:
+                continue
+            if message_text =="/start":
+                text = "👋 Привет!  Это Telegram-бот для подключения к VPN. Вам доступен бесплатный период - 10 дней. Для начала работы нажмите в меню кнопку ⚡️Подключиться ↓"
+                reply_buttons = [
+                    ["ℹ️ Статус", "⚡️ Подключиться"],
+                    ["🔥 Купить", "❓ Помощь"],
+                ]
+                send_message(
+                    chat_id=chat_id,
+                    text=text,
+                    reply_buttons=reply_buttons,
+                )
+    
+            elif message_text in "ℹ️ Статус":
+                delete_message(chat_id, message_id)
+            elif message_text == "⚡️ Подключиться":
+                delete_message(chat_id, message_id)
+            elif message_text == "🔥 Купить":
+                delete_message(chat_id, message_id)
+            elif message_text == "❓ Помощь":
+                delete_message(chat_id, message_id)
+                text = "Если у вас проблемы с подключением, отправьте статус из бота и скриншот из приложения, которым вы пользуетесь для доступа к VPN в поддержку.Ниже представлены инструкции для подключения к сервису ↓"
+                url_buttons = [
+                    ("Подключить iOS/MacOS 🍏", "https://telegra.ph/Podklyuchenie-v2raytun-iOS-11-09"),
+                    ("ПОдключить Android 🤖", "https://telegra.ph/Podklyuchenie-v2RayTun-Android-11-09"),
+                    ("Подключить Windows 🖥️", "https://telegra.ph/Nastrojka-VPN-PK-Windows-08-08"),
+                    ("🆘Поддержка", "https://web.telegram.org/k/#@artydevx"),
+                ]
+                callback_buttons = [("Назад")]
+                send_message(chat_id=chat_id, text=text, inline_url_buttons=url_buttons)
+                send_message(chat_id=chat_id, text=text, inline_callback_buttons=callback_buttons)
+
+    except Exception as e:
+        print(f"The error is {e}")
+
+    time.sleep(2)
+
---
+++ bot/api_client.py
@@ -0,0 +1,78 @@
+import requests
+import json
+from bot.config_reader import env_config
+
+token = env_config.telegram_token.get_secret_value()
+
+def reply_keyboard_builder(buttons: list) -> str | None:
+    if not buttons:
+        return None
+    return json.dumps({
+        "keyboard": buttons, 
+        "is_persistent": True,
+        "one_time_keyboard": False,
+        })
+
+
+def inline_keyboard_builder(buttons: list) -> str | None:
+    if not buttons:
+        return None
+    result = []
+    for text, url in buttons:
+        result.append(
+            {
+                "text": text,
+                "url": url,
+            }
+        )
+
+    return json.dumps({"inline_keyboard": [result]})
+
+
+def inline_keyboard_callbacks_builder(buttons: list) -> str | None:
+    if not buttons:
+        return None
+    result = []
+    for text, callback_data in buttons:
+        result.append(
+            {
+                "text": text,
+                "callback_data": callback_data,
+            }
+        )
+
+    return json.dumps({"inline_keyboard": [result]})
+
+def send_message(chat_id: int, text: str, reply_buttons=None, inline_url_buttons=None, inline_callback_buttons=None):
+    params = {
+        "chat_id": chat_id,
+        "text": text,
+    }
+
+    if reply_buttons:
+        params["reply_markup"] = reply_keyboard_builder(reply_buttons)
+    elif inline_url_buttons:
+        params["reply_markup"] = reply_keyboard_builder(inline_url_buttons)
+    elif inline_callback_buttons:
+        params["reply_markup"] = reply_keyboard_builder(inline_callback_buttons)
+
+    requests.post(f"https://api.telegram.org/bot{token}/sendMessage", params=params)
+  
+
+def delete_message(chat_id: int, message_id: int):
+    requests.post(
+        f"https://api.telegram.org/bot{token}/deleteMessage",
+        params={
+            "chat_id": chat_id,
+            "message_id": message_id,
+        }
+    )
+
+
+def get_updates(next_update_id: int):
+    return requests.get(
+            f"https://api.telegram.org/bot{token}/getUpdates",
+            params={
+                "offset": next_update_id,
+            },
+        )
