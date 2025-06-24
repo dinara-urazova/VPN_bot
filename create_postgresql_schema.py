1612a95ad30b884d46992e8d2d788d6e9ebb0165
Oleg Sklyarov <oleg.sklyarov@gmail.com>
2025-06-24 16:18:58 +0300

    создание БД + логи в консоли

--- create_postgresql_schema.py
+++ create_postgresql_schema.py
@@ -1,29 +1,42 @@
 import pg8000
+
 from bot.config_reader import env_config
 
-connection = pg8000.connect(
+with pg8000.connect(
     user=env_config.postgresql_username,
-    password=env_config.postgresql_password,
+    password=env_config.postgresql_password.get_secret_value(),
+    host=env_config.postgresql_hostname,
+    port=env_config.postgresql_port,
+) as connection:
+    print(f"[info] Connected to PostgreSQL {env_config.postgresql_hostname}:{env_config.postgresql_port}")
+    print(f"[info] Trying to create database {env_config.postgresql_database}...")
+    try:
+        connection.execute_simple(f"CREATE DATABASE {env_config.postgresql_database}")
+        print("[info] OK")
+    except Exception as e:
+        print(f"[warning] {e}")
+
+with pg8000.connect(
+    user=env_config.postgresql_username,
+    password=env_config.postgresql_password.get_secret_value(),
     host=env_config.postgresql_hostname,
     port=env_config.postgresql_port,
     database=env_config.postgresql_database,
-)
-
-cursor = connection.cursor()
-
-sql_create_table = """
-CREATE TABLE IF NOT EXISTS users (
-    telegram_id BIGINT PRIMARY KEY NOT NULL,
-    first_name VARCHAR(255) NOT NULL, 
-    last_name VARCHAR(255) NULL,
-    username VARCHAR(255) NULL, 
-    created_at TEXT, 
-    updated_at TEXT
-    );
-"""
-
-cursor.execute(sql_create_table)
-connection.commit()
-
-cursor.close()
-connection.close()
+) as connection:
+    print(f"[info] Connected to PostgreSQL {env_config.postgresql_hostname}:{env_config.postgresql_port}/{env_config.postgresql_database}")
+    sql_create_table = """
+    CREATE TABLE IF NOT EXISTS users (
+        telegram_id BIGINT PRIMARY KEY NOT NULL,
+        first_name VARCHAR(255) NOT NULL,
+        last_name VARCHAR(255) NULL,
+        username VARCHAR(255) NULL,
+        created_at TEXT,
+        updated_at TEXT
+        );
+    """
+    print(f"[info] Trying to create table users...")
+    try:
+        connection.execute_simple(sql_create_table)
+        print("[info] OK")
+    except Exception as e:
+        print(f"[error] {e}")
